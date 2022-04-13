521202e6e566be7d8ba1a39a1771ef12ac5e444a
unknown <aqil@intermediatech.id>
2022-04-13 23:31:35 +0700

    final ldr

--- intensitas cahaya/src/main.cpp
+++ intensitas cahaya/src/main.cpp
@@ -1,24 +1,56 @@
 #include <Arduino.h>
+#include "DHT.h"
 #define sensorLDR A0
+#define DHTPIN D1
+#define DHTTYPE DHT11
 int nilaiSensor;
-#define kuning D3
+#define biru D3
+#define abang D2
+DHT dht(DHTPIN, DHTTYPE);
 void setup() {
  Serial.begin(115200);
- pinMode(kuning, OUTPUT);
+ pinMode(biru, OUTPUT);
+ pinMode(abang, OUTPUT);
  Serial.println("Contoh Penggunaan Sensor LDR");
- delay(3000);
+ dht.begin();
 }
-
 void loop() {
  nilaiSensor = analogRead(sensorLDR);
- Serial.print("Nilai Sensor : ");
- Serial.println(nilaiSensor);
- if(nilaiSensor > 400){
-   Serial.println(F("Sore / Malam beh!"));
-   digitalWrite(kuning, HIGH);
- }else{
-   Serial.println(F("Pagi beh!"));
-   digitalWrite(kuning, LOW);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float f = dht.readTemperature(true);
+
+  if (isnan(h) || isnan(t) || isnan(f))
+  {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  Serial.print("Nilai Sensor Cahaya: ");
+  Serial.println(nilaiSensor);
+
+  Serial.print(F("Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+
+ if(nilaiSensor > 400 && t < 28){
+   Serial.println(F("Cahaya redup dan suhu dingin"));
+   digitalWrite(biru, HIGH);
+   delay(50);
+   digitalWrite(biru,LOW);
+ }else if(nilaiSensor > 400 && t > 28){
+   Serial.println(F("Cahaya redup dan suhu tinggi"));
+   digitalWrite(biru, HIGH);
+   delay(50);
+   digitalWrite(biru,LOW);
+ }else if(nilaiSensor < 400 && t < 28){
+   Serial.println(F("Cahaya terang dan suhu dingin"));
+   digitalWrite(abang, HIGH);
+   delay(50);
+   digitalWrite(abang,LOW);
+ }else if(nilaiSensor < 400 && t > 28){
+   Serial.println(F("Cahaya terang dan suhu tinggi"));
+   digitalWrite(biru, HIGH);
+   digitalWrite(abang,LOW);
  }
     delay(2000);
 }
